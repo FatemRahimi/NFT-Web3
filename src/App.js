008c0ced6cb03af2619c4476ecaaa8845144997a
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-08-03 23:04:59 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ function App() {
                 ref={mintAmountRef}
                 className="mx-2"
                 type="number"
-                defaultValue={2}
+                defaultValue={1}
                 min={1}
                 max={5}
               />
